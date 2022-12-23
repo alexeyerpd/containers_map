4f1619a6913dc8f3eb5b04452b437ced8fc92dd1
Alexey Pochekutov <alexeyerpd@gmail.com>
2022-12-23 04:26:22 +0300

    fix lint

--- src/js/__tests__/errorRepository.test.js
+++ src/js/__tests__/errorRepository.test.js
@@ -1,11 +1,11 @@
-import {ErrorRepository} from '../errorRepository';
+import { ErrorRepository } from '../errorRepository';
 
-const errorRepository = new ErrorRepository;
+const errorRepository = new ErrorRepository();
 
 test('ErrorRepository - get error message', () => {
-    expect(errorRepository.translate(1)).toBe('Ошибка 1');
-})
+  expect(errorRepository.translate(1)).toBe('Ошибка 1');
+});
 
 test('ErrorRepository - get default error message ', () => {
-    expect(errorRepository.translate(100)).toBe('Unknown error');
-})
+  expect(errorRepository.translate(100)).toBe('Unknown error');
+});
